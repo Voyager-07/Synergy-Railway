db4bb2334ad2e3141be526470ccd5b1a4bb178c4
Abhijeet Srivastava <118682453+Voyager-07@users.noreply.github.com>
2025-03-28 16:52:28 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -20,12 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)oftfzx+t!7o2et28&9@gidnk0jb129ivka0)ndnn3dt=wo63y'
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'synergy.up.railway.app']
 
 
 # Application definition
